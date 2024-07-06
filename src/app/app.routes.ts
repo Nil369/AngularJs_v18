5d0b40959ad6be57a89480623ccfbab8c82e936b
Akash Halder <halderakash872@gmail.com>
2024-07-06 19:05:17 +0530

    added pipes & home component, added Animations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { DatabindingComponent } from './components/databinding/databinding.component';
 import { StructuralDirectiveComponent } from './components/directives/structural-directive/structural-directive.component';
 import { AttributeDirectiveComponent } from './components/directives/attribute-directive/attribute-directive.component';
+import { PipeComponent } from './components/pipe/pipe.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
     {
         path:'data-binding',
         component:DatabindingComponent
@@ -15,5 +21,9 @@ export const routes: Routes = [
     {
         path: 'attributedirective',
         component: AttributeDirectiveComponent
+    },
+    {
+        path: 'pipes',
+        component: PipeComponent
     },
 ];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,68 @@
+import { Router } from '@angular/router';
+import { AfterViewInit, Component } from '@angular/core';
+import { gsap } from 'gsap';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements AfterViewInit {
+
+  constructor(private router: Router) {
+
+  }
+
+  ngAfterViewInit() {
+    // Animate the image from left to right with opacity 0 to 1
+    gsap.fromTo('#hero-image', 
+      {
+        x: -200,
+        opacity: 0,
+        borderRadius: '0%',
+      },
+      {
+        duration: 1.5,
+        x: 0,
+        opacity: 1,
+        borderRadius: '50%',
+        ease: 'power3.out'
+      }
+    );
+
+    // Stagger the text animation
+    gsap.from('#hero-text', {
+      duration: 1,
+      delay: 1,
+      y: 50,
+      opacity: 0,
+      ease: 'power3.out'
+    });
+
+    gsap.from('#hero-description', {
+      duration: 1,
+      delay: 1.5,
+      y: 50,
+      opacity: 0,
+      ease: 'power3.out',
+    });
+
+    // Animate the button gradient color
+    gsap.from('#explore-button', {
+      backgroundPosition: '200% center',
+      duration: 1.5,
+      delay: 1,
+      y: 50,
+      opacity: 0,
+      ease: 'power3.out',
+      
+    });
+  }
+
+  naviagteToAttribute() {
+    this.router.navigateByUrl("data-binding")
+  }
+
+}
